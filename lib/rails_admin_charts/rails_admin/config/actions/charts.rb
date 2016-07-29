87719ccff6dbaf3775ddca1725775862f5b75da3
Marco Longhitano <marcovlonghitano@gmail.com>
2016-07-29 14:42:43 +0200

    csv export added

--- lib/rails_admin_charts/rails_admin/config/actions/charts.rb
+++ lib/rails_admin_charts/rails_admin/config/actions/charts.rb
@@ -16,6 +16,32 @@ module RailsAdmin
           [:get, :post, :delete]
         end
 
+        register_instance_option :controller do
+          proc do
+            if params["chart_form"].present?
+              # @abstract_model.model.graphs_data.each_with_index do |value, index|
+              #   data = @abstract_model.model.graph_data(params[:chart_form][:from],
+              #     params[:chart_form][:to],
+              #     @abstract_model.model.sym_for_condition => params[:chart_form][:condition])
+              #   render partial: "chart", formats: [:html], locals: {data: data, index: index}
+              # end
+              render "charts"
+            else
+              respond_to do |format|
+                format.html do
+                  render action: @action.template_name
+                end
+                format.xls
+                format.csv do
+                  response.headers['Content-Type'] = 'text/csv'
+                  response.headers['Content-Disposition'] = "attachment; filename=.#{@abstract_model.model.name.downcase}csv"
+                  render :template => "rails_admin/csv/#{@abstract_model.model.name.downcase}.csv.erb"
+                end
+              end
+              #render action: @action.template_name
+            end
+          end
+        end
 
         register_instance_option :pjax? do
           false
